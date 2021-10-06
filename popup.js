45bf04bedf874814a71c62e3e9995172d829e896
Fabio <f.cioni612@gmail.com>
2021-10-06 22:47:23 +0200

    add storage to store 'active' variable

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
 const checkbox = document.getElementById("checkbox");
+const slider = document.getElementsByClassName("slider")[0];
+
+chrome.storage.sync.get("active", ({ active }) => {
+    checkbox.checked = active;
+});
 
 checkbox.addEventListener("click", () => {
-    alert("works");
+    chrome.storage.sync.get("active", ({ active }) => {
+    chrome.storage.sync.set({ active: !active });
+    });
 });
